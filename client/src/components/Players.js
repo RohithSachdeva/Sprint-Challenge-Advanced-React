e430770bd63bbc76ef27aca17d260f5a9d51e343
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-04-10 13:20:51 -0700

    resubmit

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -5,7 +5,7 @@ export default function Players(props) {
     console.log(props);
 
     return (
-        <div className='box'>
+        <div className='box' data-testid ="render">
             <h2>{props.user.name}</h2>
             <h3>{props.user.country}</h3>
             <h4>{props.user.searches}</h4>
